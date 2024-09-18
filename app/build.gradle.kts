c35282ce7fe42cebc1dc20369303da18d7cbde01
Max Sveshnikov <msveshnikov@gmail.com>
2024-09-18 19:41:57 +0200

    Minor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.maxsoft.stepstracker"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
